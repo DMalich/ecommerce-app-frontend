e00ebe8607e2e7a314b88fa3f633d12f709c2f33
DMalich <d.malich24@gmail.com>
2023-03-20 16:56:07 +0200

    upper header created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
+import '../App.css';
 import '../styles/header.css';
 
 const Header = () => {
@@ -21,14 +23,57 @@ const Header = () => {
             </header>
             <header className='header-upper'>
                 <div className="container">
-                    <div className="row">
+                    <div className="row align-items-center py-3">
                         <div className="col-2">
                             <h2>
                                 <Link className='text-white'>Shop</Link>
                             </h2>
                         </div>
-                        <div className="col-5"></div>
-                        <div className="col-5"></div>
+                        <div className="col-5">
+                            <div className='input-group'>
+                                <input
+                                    type='text'
+                                    className='form-control py-2'
+                                    placeholder='Search here...'
+                                    aria-label='Search here...'
+                                    aria-describedby='basic-addon2'
+                                />
+                                <span className='input-group-text p-2' id='basic-addon2'>
+                                    <BsSearch className='fs-5'/>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="col-5">
+                            <div className="header-upper-links d-flex align-items-center justify-content-between">
+                                <div>
+                                    <Link className='d-flex align-items-center text-white gap-10'>
+                                        <img src='images/compare.svg' alt='Compare' />
+                                        <p className='mb-0'>Compare <br/> Products</p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center text-white gap-10'>
+                                        <img src='images/wishlist.svg' alt='Wishlist' />
+                                        <p className='mb-0'>Wishlist</p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center text-white gap-10'>
+                                        <img src='images/user.svg' alt='User' />
+                                        <p className='mb-0'>My Account</p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center text-white gap-10'>
+                                        <img src='images/cart.svg' alt='Cart' />
+                                        <div className='d-flex flex-column'>
+                                            <span className='badge bg-white text-dark'>0</span>
+                                            <p className='mb-0'>$ 50</p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </header>
